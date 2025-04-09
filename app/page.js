9c46e3925d51e8df7bae34e0714d5b89bb20b9d8
Paresh Patil <paresh2004patil@gmail.com>
2025-04-09 08:33:11 +0530

    intial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Welcome To Pragya Ai 2.0</h2>
+      <Button>Get Started</Button>
+    </div>
+  );
+}
